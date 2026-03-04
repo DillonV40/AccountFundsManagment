e3bea86efa6dfe351be16fc654963e2a28d182a5
DillonV40 <dillon.vaughan346@gmail.com>
2026-03-03 19:31:59 -0600

    first commit

---
+++ src/main/java/djv/account/management/AccountManagementApplication.java
@@ -0,0 +1,13 @@
+package djv.account.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementApplication.class, args);
+	}
+
+}
